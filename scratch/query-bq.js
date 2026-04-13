3ae490cf93ff391356bd309425b2c8256a57ce49
Kenichiro Takamatsu <ken@KenichironoMacBook-Air.local>
2026-04-13 10:06:27 +0700

    chore: ignore scratch/debug files and untrack accidentally committed files

--- scratch/query-bq.js
+++
@@ -1,7 +0,0 @@
-const { BigQuery } = require("@google-cloud/bigquery");
-const bq = new BigQuery();
-async function run() {
-  const [rows] = await bq.query("SELECT * FROM \`heat_ranking.artists_master\` LIMIT 2");
-  console.log(JSON.stringify(rows, null, 2));
-}
-run();
